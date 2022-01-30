1382315843e3bae20ef4ab2ab1571815ecadbc0d
Chris Pucknell <chris.pucknell@gmail.comgit config --global -lgit config --global user.email chris.pucknell@gmail.com>
2022-01-30 19:27:50 +0000

    Re-working project file structure!

--- scripts/index.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('login-button').onclick = () => location.href = "./login.html";
-
-/*
-const loginButton = document.getElementById('login-button');
-loginButton.onclick = function() {
-    location.href = "./login.html";
-};
-*/
